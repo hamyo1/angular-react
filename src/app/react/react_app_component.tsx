1a5bfeae9aa4c0682284bc0108125b15cf905bba
hamyo1 <48315227+hamyo1@users.noreply.github.com>
2022-04-24 13:13:18 +0300

    Update react_app_component.tsx

--- src/app/react/react_app_component.tsx
+++ src/app/react/react_app_component.tsx
@@ -7,22 +7,70 @@ import HeaderCompnent from './reactComponents/tsx/react_HeaderCompnent';
 import AddDiamondCompnent from './reactComponents/tsx/react_AddDiamondCompnent';
 import toast, { Toaster } from 'react-hot-toast';
 import { useState } from 'react';
+import { Diamond } from 'angular-react/src/app/shared/models/diamond.model';
+import ErrorPageCompnent from './reactComponents/tsx/react_errorPageCompnent';
 
 
-const React_App_Component = (diamondsListFirst: DiamondsList) => {
+const React_App_Component = () => {
 
-    const [diamondsList, setTheArray] = useState<DiamondsList>(diamondsListFirst);
+    const [diamondsList, setTheArray] = useState<DiamondsList>([{} as Diamond]);
+    const [pageStatus, setPageStatus] = useState<number>(1);
+    
+    React.useEffect(() => {
+        var diamondsListFromFetch: DiamondsList=[];
+        //get all data (the better way to that in big data is by paging)
+        fetch('http://localhost:4200/GetAllDiamonds', {
+            method: 'get',
+            headers: {
+              "Accept": "application/json",
+              "Content-type": "application/json"
+            },
+          })
+            .then(res => {
+              if(res.ok)
+              {
+                return res.json();
+              }
+              console.log(`error when trying get data: api return error status: ${res.status}`);
+              throw res.statusText;
+            })
+            .then(resp => {
+              if(resp.errorCode=='500'){
+                throw resp.errorDesc;
+              }
+              resp.forEach((element: Diamond) => {
+                diamondsListFromFetch.push(element);
+              });
+              //(render again)
+              setTheArray(diamondsListFromFetch);
+              setPageStatus(1);
+          }).catch(err =>
+            {console.log(`error when trying get data: ${err}`);
+            setPageStatus(2);});
+      }, []);
+    
+    //i can send the 'setPageStatus' in props for more error handling (i was do that 'PageStatus' 2 it is 'loadingPage' but from now i choose to delete it)
+    //i am considering another way to handle inside dives and not just global
+    if(pageStatus==1)//sucsess
+    {
+        return (
+        
+            <div className="container-fluid h-100 d-flex flex-column justify-content-start">
+                <HeaderCompnent />
+                <StatisticCompnent diamondsList={diamondsList} />
+                <GridAllDiamondsCompnent diamondsList={diamondsList} />
+                <AddDiamondCompnent setTheArray={setTheArray} diamondsList={diamondsList} />
+                <Toaster/>
+            </div>
+        )
+    }
+    else//error
+    {
+        return(
+        <ErrorPageCompnent/>
+        )
+    }
 
-    return (
-
-        <div className="container-fluid h-100 d-flex flex-column justify-content-start" >
-            <HeaderCompnent />
-            <StatisticCompnent diamondsList={diamondsList} />
-            <GridAllDiamondsCompnent diamondsList={diamondsList} />
-            <AddDiamondCompnent setTheArray={setTheArray} diamondsList={diamondsList} />
-            <Toaster/>
-        </div>
-    )
 }
 
 export default React_App_Component;
