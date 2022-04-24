c8c88dc3f1b67bc63d6ecbe4dc78ba22c8f44590
hamyo1 <48315227+hamyo1@users.noreply.github.com>
2022-04-24 13:12:53 +0300

    Update app.component.tsx

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -10,7 +10,7 @@ import { Diamond, DiamondsList } from './shared/models/diamond.model';
 
 @Component({
   selector: 'app-root',
-  template: '<div [id]="rootId"></div>',
+  template: '<div [id]="rootId"></div>',//(inject react root here)
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnChanges, AfterViewInit, OnDestroy  {
@@ -21,9 +21,7 @@ export class AppComponent implements OnChanges, AfterViewInit, OnDestroy  {
 
 
   ngOnChanges(changes: SimpleChanges) {
-    debugger;
     this.render();
-
   }
 
    async ngAfterViewInit() {
@@ -33,48 +31,9 @@ export class AppComponent implements OnChanges, AfterViewInit, OnDestroy  {
   ngOnDestroy() {
 
   }
-
-  private async render() {
-    var diamondsList: DiamondsList= await this.GetAllDiamonds();
-    if(diamondsList.length>0)
-    {
-      ReactDOM.render(React.createElement(React_App_Component,diamondsList), document.getElementById(this.rootId));
-    }
-    else
-    {
-      ReactDOM.render(React.createElement(ErrorPageCompnent), document.getElementById(this.rootId));
-    }
+//rendering react React_App_Component
+  private render() {
+    ReactDOM.render(React.createElement(React_App_Component), document.getElementById(this.rootId));
   }
-
-  async GetAllDiamonds() {
-    var diamondsList1: DiamondsList=[]
-   await fetch('http://localhost:4200/GetAllDiamonds', {
-    method: 'get',
-    headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
-    },
-    // (var) payload looks like this {Header: "Sending", . . .} 
-  })
-    .then(res => {
-      if(res.ok)
-      {
-        return res.json();
-      }
-      alert(`error when trying get data: api return error status: ${res.status}`);
-      throw res.statusText;
-    })
-    .then(resp => {
-      if(resp.errorCode=='500'){
-        throw resp.errorDesc;
-      }
-      resp.forEach((element: Diamond) => {
-        diamondsList1.push(element);
-      });
-      //return diamondsList1;
-  }).catch(err =>alert(`error when trying get data: ${err}`));
-  
-    return diamondsList1;
-}
 
 }
